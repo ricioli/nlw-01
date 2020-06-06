a98eee74de9ecb9e991b7f6b84c48ad0aae9cf49
Alex Fabiano Ricioli <alexricioli@outlook.com>
2020-06-06 14:49:33 -0300

    Commit inicial

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import path from 'path';
+const app = express();
+import { errors } from 'celebrate';
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
+
+app.listen(3333, () => {
+  console.log('escutando na porta 3333');
+})
